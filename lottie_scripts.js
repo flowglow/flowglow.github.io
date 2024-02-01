e8e8b37f2a69ffce8b0e78a107088a1e9160bd3f
ZHURAVLEV Roman <Roman.ZHURAVLEV@Saipem.com>
2024-02-01 14:58:14 +0100

    changed structure

---
+++ lottie_scripts.js
@@ -0,0 +1,11 @@
+import { Lottie } from '@lottiefiles/lottie-js'; // Import Lottie library
+
+const animation = Lottie.loadAnimation({
+    container: document.getElementById('lottie-container'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'lottie_anims/loading.json' // Path to your Lottie animation JSON file
+});
+
+// Additional JavaScript code for your website can be added here
