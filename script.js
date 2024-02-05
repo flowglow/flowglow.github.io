1e7e6c38fd0dcad6fcbc57f9fd639012cfe1408e
ZHURAVLEV Roman <Roman.ZHURAVLEV@Saipem.com>
2024-02-05 15:08:35 +0100

    fonts change

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+const fonts = [
+    'Kirang Haerang',
+    'Indie Flower',
+    'Rye',
+    'Amatic SC',
+    'Bangers',
+    'Fredericka the Great'
+]
+
+/* const fonts = [
+    'Optima',
+    'Georgia',
+    'Comic Sans',
+    'Impact',
+    'Garamond',
+    'Bookman'
+] */
+
+const words = document.querySelectorAll('.word');
+
+let count=0;
+
+const changeFonts = () => {
+    words.forEach(word => {
+        let randomFontIndex = Math.floor(Math.random() * fonts.length);
+        let randomFont = fonts[randomFontIndex];
+        word.style.fontFamily = randomFont;
+    })
+}
+
+function rollIntro(){
+    changeFonts();
+    setTimeout(rollIntro, 1000);
+}; rollIntro();
+
 var animation = bodymovin.loadAnimation({
     container: document.getElementById('lottie-animation'),
     renderer: 'svg',
@@ -5,3 +40,5 @@ var animation = bodymovin.loadAnimation({
     autoplay: true,
     path: 'lottie_anims/loading.json' // Path to your Lottie animation JSON file
 });
+
+
