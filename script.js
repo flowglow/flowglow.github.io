7afd777ebc3b3b3eb59d836d53e9d6ebfdc00f93
ZHURAVLEV Roman <Roman.ZHURAVLEV@Saipem.com>
2024-02-05 11:46:21 +0100

    back to roots

--- script.js
+++ script.js
@@ -1,30 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Load the Lottie animation
-    const animation = lottie.loadAnimation({
-        container: document.getElementById('lottie-animation'),
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        path: 'lottie_anims/loading.json'
-    });
-
-    // Function to handle scroll events
-    function handleScroll() {
-        // Get the Lottie animation element
-        let lottieAnimation = document.getElementById("lottie-animation");
-        // Get the Instagram video element
-        let instagramVideo = document.getElementById("instagram-video");
-        // Get the current scroll position
-        let scrollPosition = window.scrollY;
-
-        let opacity = 1 - (scrollPosition / window.innerHeight); // Adjust this formula as needed
-
-        // Apply the opacity to the Lottie animation
-        lottieAnimation.style.opacity = opacity;
-        instagramVideo.style.opacity = 1 - opacity;
-
-    }
-
-    // Add scroll event listener
-    window.addEventListener("scroll", handleScroll);
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('lottie-animation'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'lottie_anims/loading.json' // Path to your Lottie animation JSON file
 });
