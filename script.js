41a5fbd075e0830619b29aeb3f2545c723c17c2a
ZHURAVLEV Roman <Roman.ZHURAVLEV@Saipem.com>
2024-02-05 18:08:28 +0100

    fade in out

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const fonts = [
     'Rye',
     'Amatic SC',
     'Bangers',
-    'Fredericka the Great'
+    'Fredericka the Great',
+    'Bungee Inline'
 ]
 
 /* const fonts = [
@@ -16,22 +17,54 @@ const fonts = [
     'Bookman'
 ] */
 
+const runwords = true
 const words = document.querySelectorAll('.word');
+const letters = document.querySelectorAll('.letter');
 
-let count=0;
-
-const changeFonts = () => {
-    words.forEach(word => {
+const changeFonts = (obj_list) => {
+    obj_list.forEach(obj => {
         let randomFontIndex = Math.floor(Math.random() * fonts.length);
         let randomFont = fonts[randomFontIndex];
-        word.style.fontFamily = randomFont;
+        obj.style.fontFamily = randomFont;
     })
 }
 
-function rollIntro(){
-    changeFonts();
+function rollLetter(){
+    changeFonts(letters);
+    setTimeout(rollLetter, 1000);
+};
+
+/* function rollIntro(){
+    changeWordFonts();
     setTimeout(rollIntro, 1000);
-}; rollIntro();
+}; rollIntro(); */
+
+/* let introAnimation = setInterval(function() {
+    changeFonts(words);
+    if(count>30)
+        clearInterval(introAnimation);
+    count++;
+}, 300) */
+
+let interval = setInterval(function(){
+    changeFonts(words);
+}, 1000);
+
+setTimeout(() => clearInterval(interval), 5000)
+
+var the_word = document.getElementById('the-word');
+setTimeout(function(){
+    $(`#loading-word`).fadeOut(1000);
+    $(`#lottie-animation`).fadeOut(1000);
+    the_word.style.fontFamily = 'Amatic SC'
+    // $(`#the-word`).fadeOut(1000);
+    rollLetter();
+}, 7000);
+
+setTimeout(function(){
+    $(`#instagram-video`).fadeIn(2000);
+}, 8500)
+
 
 var animation = bodymovin.loadAnimation({
     container: document.getElementById('lottie-animation'),
