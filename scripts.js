ccfaea55c60e786b0151ed3fb8c7bd7f669a0dde
ZHURAVLEV Roman <Roman.ZHURAVLEV@Saipem.com>
2024-03-05 18:53:25 +0100

    lottie added

--- scripts.js
+++ scripts.js
@@ -9,3 +9,11 @@ navToggle.addEventListener("click", () => {
   primaryNav.toggleAttribute("data-visible");
   primaryHeader.toggleAttribute("data-overlay");
 });
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('lottie-animation'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'src/lottie/Wanna_Motion_logo_site.json'
+});
